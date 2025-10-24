429ba586794a23d57b6483b3d2bdb438cc6c195a
sarah elkholy <saraelkholyarmy03@gmail.com>
2025-10-24 21:47:56 +0300

    - created api constants file - created api error handler - created api error model & result - created api service - creayed dio factory

---
+++ lib/core/networking/api_service.dart
@@ -0,0 +1,17 @@
+import 'package:dio/dio.dart';
+import 'package:fitnestx/core/networking/api_constants.dart';
+
+import 'package:retrofit/error_logger.dart';
+import 'package:retrofit/http.dart';
+part 'api_service.g.dart';
+
+@RestApi(baseUrl: ApiConstants.apiBaseUrl)
+abstract class ApiService {
+  factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  // @POST(ApiConstants.login)
+  // Future<LoginResponse> login(@Body() LoginRequestBody loginRequestBody);
+
+  // @POST(ApiConstants.signup)
+  // Future<SignUpResponse> signup(@Body() SignUpRequestBody signUpRequestBody);
+}
